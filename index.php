e3ded1f798fa3ab72380fe6c30b966a4cb6c29d9
emilezi <75627932+emilezi@users.noreply.github.com>
2026-08-06 13:12:44 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -4,6 +4,7 @@ require 'config/db_connexion.php';
 require 'config/host_connexion.php';
 
 require 'class/Database.php';
+require 'class/Setting.php';
 require 'class/Session.php';
 
 require 'actions/site.php';
